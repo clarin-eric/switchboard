709331dc2b4551a2d2465a32a669b7a462f33622
Claus Zinn <claus.zinn@uni-tuebingen.de>
2018-03-27 12:51:07 +0200

    After merge with master, new development on equal par with master

--- webpack.config.js
+++ webpack.config.js
@@ -117,8 +117,8 @@ const common = {
 		'ALLOW_TEXT_INPUT': JSON.stringify('yes'),
 		// check whether we allow pasting of URLs
 		'ALLOW_PASTE_URL': JSON.stringify('yes'),
-		'URL_PATH': JSON.stringify('/clrs'),
-		//'URL_PATH': JSON.stringify('/clrs-dev'),
+		//'URL_PATH': JSON.stringify('/clrs'),
+		'URL_PATH': JSON.stringify('/clrs-dev'),
 		// 'FILE_STORAGE': JSON.stringify('MPCDF'),
 		'FILE_STORAGE': JSON.stringify('NEXTCLOUD'),		
 		//'FILE_STORAGE': JSON.stringify('B2DROP'),
@@ -126,7 +126,7 @@ const common = {
 		'B2DROP_PASS' : JSON.stringify('clarin-plus'),
 		//'B2DROP_USER' : JSON.stringify('claus.zinn@uni-tuebingen.de'),
 		//'B2DROP_PASS' : JSON.stringify('sPL-Fh2-7SS-hCJ'),
-		'VERSION'     : JSON.stringify('v1.1.4-pro/docker (Mar 27, 2018 - noon)'),
+		'VERSION'     : JSON.stringify('v1.1.4-dev/docker (Mar 27, 2018 - noon)'),
 		'CONTACT'     : JSON.stringify('switchboard@clarin.eu')
 	    }
 	}),
