e73ad047df7a41c48bc7c40a4cebb4cc50ab789c
André Moreira <andre@clarin.eu>
2018-07-03 18:16:39 +0200

    Improved code comments

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,6 +3,9 @@
 rm -rf build
 mkdir -p build/CLRSwitchboard
 
+
+# Create docker image which builds the code.
+# The necessary build environment is supplied by the FROM image registry.gitlab.com/clarin-eric/docker-alpine-clrs-build_env:1.0.0
 docker build --no-cache --tag clarin/switchboard .
 
 EXISTING_CONTAINER=$(docker ps -aq --filter name=alpinelrs-build)
@@ -10,6 +13,7 @@ if [ ! -z "${EXISTING_CONTAINER}" ]; then
 	docker rm "${EXISTING_CONTAINER}"
 fi
 
+# Spin the image so we can use "docker cp"
 docker run --name alpinelrs-build clarin/switchboard
 
 docker cp alpinelrs-build:/tmp/lrs/build build/CLRSwitchboard/http
