4596a4b2b578afc8a8c6970ef75b5143c428152b
Emanuel Dima <emanueldima@gmail.com>
2020-03-10 13:16:54 +0100

    support markdown (GitHub flavor) in tools description and auth info

---
+++ webui/src/components/Highlighter.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
+
+const RE_ESCAPE_REGEX = /[-\/\\^$*+?.()|[\]{}]/g;
+
+
+function escapeRegExp(string) {
+    // $& means the whole matched string
+    return string.replace(RE_ESCAPE_REGEX, '\\$&');
+}
+
+function spanRenderer({key, str, highlight}) {
+    return (
+        <span key={key} className={highlight ? "highlight" : ""}>
+            { str }
+        </span>
+    );
+}
+
+function makeTextRenderer(terms) {
+    let splitter = null;
+    if (terms.length > 0) {
+        const re_string = "("+terms.map(escapeRegExp).join("|")+")";
+        splitter = new RegExp(re_string, 'gi');
+    }
+    return ({value}) => {
+        const text = value || '';
+        // Split on higlight term and include term into parts, ignore case
+        const parts = splitter == null ? [text] : text.split(splitter);
+        const spans = parts.map((part, i) => ({
+            key: i,
+            str: part,
+            highlight: terms.some(term => term.toLowerCase() === part.toLowerCase()),
+        }));
+        return <span> { spans.map(spanRenderer) } </span> ;
+    };
+}
+
+export function makeHighlighter(terms) {
+    const textRenderer = makeTextRenderer(terms);
+    return function({text, markdown, style}) {
+        if (text && markdown) {
+            console.warn("cannot render both text and markdown at the same time");
+        }
+        if (text) {
+            return textRenderer({value:text});
+        }
+        return (
+            <ReactMarkdown
+                source={markdown}
+                skipHtml={true}
+                escapeHtml={true}
+                renderers={{text: textRenderer}}
+            />
+        );
+    };
+};
