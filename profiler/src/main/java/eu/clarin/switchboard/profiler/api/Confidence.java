aa2e07d50b596d4a7229e40c1cbbdd4dd6bcbd58
Emanuel Dima <emanueldima@gmail.com>
2020-03-17 11:38:44 +0100

    remove profiler (moved to separate repository)

--- profiler/src/main/java/eu/clarin/switchboard/profiler/api/Confidence.java
+++
@@ -1,9 +0,0 @@
-package eu.clarin.switchboard.profiler.api;
-
-/**
- * Enumeration specifying the confidence we have that a profile is correctly detected or not.
- */
-public enum Confidence {
-    Uncertain,
-    Certain,
-}
