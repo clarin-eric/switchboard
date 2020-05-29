0a408b53f392e75d69077031caf5997778669b11
Emanuel Dima <emanueldima@gmail.com>
2020-05-29 11:20:02 +0200

    set proper url in switchboard popup

--- backend/src/main/resources/webui/popup/switchboardpopup.js
+++ backend/src/main/resources/webui/popup/switchboardpopup.js
@@ -16,9 +16,26 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 
 var $ = window.$ = Zepto;
 
+var switchboardURL = null;
+
+function setSwitchboardURL(url) {
+    if (url) {
+        switchboardURL = url;
+        return;
+    } else {
+        for (const x of document.getElementsByTagName('script')) {
+            if (x.src && x.src.includes('switchboardpopup')) {
+                const url = x.src;
+                const end = url.indexOf('/popup/');
+                switchboardURL = url.substr(0, end);
+                return;
+            }
+        }
+        console.error("Could not find automatically set the Switchboard URL. Switchboard invocations will fail.");
+    }
+}
+
 function showSwitchboardPopup(align, params) {
-    const switchboardURL = 'https://switchboard.clarin-dev.eu';
-    // const switchboardURL = 'http://localhost:8081';
     testArguments(align, params);
     buildParams(params);
     const {container, popup} = makeDomElements(align, switchboardURL, params);
@@ -118,14 +135,18 @@ function togglePopup(container) {
     }
 }
 
-// the only change to globals
-window.showSwitchboardPopup = showSwitchboardPopup;
-
-// remove global Zepto traces
+// cleanup: remove global Zepto traces
 if (typeof old$ === 'undefined') {
     delete window.$
 } else {
     window.$ = old$;
 }
+
+// set the switchboard url automatically
+setSwitchboardURL();
+
+// change globals
+window.showSwitchboardPopup = showSwitchboardPopup;
+window.setSwitchboardURL = setSwitchboardURL;
 
 })();
