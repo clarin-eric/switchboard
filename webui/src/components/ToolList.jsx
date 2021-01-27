eb62549c09a0240691ff8c90595928c3f7e128b4
Emanuel Dima <emanueldima@gmail.com>
2021-01-27 14:54:56 +0100

    show badge for unsafe tools

--- webui/src/components/ToolList.jsx
+++ webui/src/components/ToolList.jsx
@@ -280,6 +280,10 @@ class ToolCard extends React.Component {
                             : <div className="badge-holder standalone" title="This tool requires local installation on one of your devices, please check the details.">
                                 <span className={"fa fa-download"} aria-hidden="true"/>
                             </div> }
+                        { tool.webApplication && tool.webApplication.url && tool.webApplication.url.startsWith("http://") ?
+                            <div className="badge-holder unsafe" title="The connection to this tool is not secure. Unauthorized parties can spy on your data.">
+                                <span className={"fa fa-eye"} aria-hidden="true"/>
+                            </div>: null }
                     </div>
                 </div>
             </div>
