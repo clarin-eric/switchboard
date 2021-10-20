68b824e288e53446585f62ea4581972311792251
Emanuel Dima <emanueldima@gmail.com>
2021-10-20 11:02:25 +0200

    bug fix: main input container clears all resources

--- webui/src/containers/InputContainer.jsx
+++ webui/src/containers/InputContainer.jsx
@@ -3,16 +3,30 @@ import React from 'react';
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {uploadFile, uploadLink} from '../actions/actions';
+import {uploadFile, uploadLink, clearResources} from '../actions/actions';
 import {Input} from '../components/Input';
 
 const mapStateToProps = (state) => ({});
+
+
 const mapDispatchToProps = (dispatch) => ({
     onFile: (file) => dispatch(uploadFile(file)),
     onLink: (link) => dispatch(uploadLink(link)),
 });
-
 export const InputContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(Input));
+
+
+const mapDispatchToPropsWithCleaning = (dispatch) => ({
+    onFile: (file) => {
+        dispatch(clearResources())
+        dispatch(uploadFile(file))
+    },
+    onLink: (link) => {
+        dispatch(clearResources())
+        dispatch(uploadLink(link))
+    },
+});
+const CleanInputContainer = withRouter(connect(mapStateToProps, mapDispatchToPropsWithCleaning)(Input));
 export const MainInputContainer = () => {
-    return <InputContainer title="Add your data"/>;
+    return <CleanInputContainer title="Add your data" />;
 };
