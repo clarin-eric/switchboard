06947aec43baaf432a2c02903d3129d7d25cbb86
Emanuel Dima <emanueldima@gmail.com>
2021-12-13 12:14:43 +0100

    use github actions to fetch tools

--- test/run-end-to-end.sh
+++ test/run-end-to-end.sh
@@ -1,9 +1,4 @@
 set -e
-if ! docker volume inspect toolsdata >/dev/null; then 
-    docker volume create toolsdata && \
-    docker run --rm  -w / -v toolsdata:/switchboard-tool-registry alpine/git \
-        clone https://github.com/clarin-eric/switchboard-tool-registry/ --branch production --depth 1
-fi
 
 test -e test/Dockerfile || (echo "This script must be run from the project root" && exit 1)
 docker build . -f test/Dockerfile -t switchboard/switchboard-tester
