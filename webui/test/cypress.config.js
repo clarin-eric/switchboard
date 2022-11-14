3cb5e0546c1b7be1babd5fd5c1a7747b93d363aa
André Moreira <amoreira.mail@gmail.com>
2022-11-14 16:13:55 +0100

    Update cypress config

---
+++ webui/test/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')module.exports = defineConfig({
+  watchForFileChanges: true,
+  defaultCommandTimeout: 10000,
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    setupNodeEvents(on, config) {
+      on('<event>', (arg1, arg2) => {
+        // plugin stuff here
+      })
+    }
+  }
+})
