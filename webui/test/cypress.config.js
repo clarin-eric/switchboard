14833e0d15cff3c0ffb351f6a82b0e620902019a
André Moreira <amoreira.mail@gmail.com>
2024-02-05 19:30:59 +0100

    Increase cypress command timeout

--- webui/test/cypress.config.js
+++ webui/test/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   watchForFileChanges: true,
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 100000,
   e2e: {
     baseUrl: 'http://localhost:8080'
   }
