6e00121e722a612e56bbc8e6ce6b277e8c511e77
André Moreira <amoreira.mail@gmail.com>
2024-02-05 21:39:11 +0100

    Use 127.0.0.1

--- webui/test/cypress.config.js
+++ webui/test/cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   watchForFileChanges: true,
   defaultCommandTimeout: 100000,
   e2e: {
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://127.0.0.1:8080'
   }
 })
