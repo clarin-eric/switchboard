28576c11e18b21a78ccbd9c30625dc53e2fb2d3e
André Moreira <amoreira.mail@gmail.com>
2024-02-05 23:24:13 +0100

    Lower cypress defaultCommandTimeout

--- webui/test/cypress.config.js
+++ webui/test/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   watchForFileChanges: true,
-  defaultCommandTimeout: 100000,
+  defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'http://127.0.0.1:8080'
   }
