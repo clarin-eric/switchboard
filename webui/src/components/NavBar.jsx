12d9ed65a73bddf294a6db9d34ac913ed9350ac5
André Moreira <amoreira.mail@gmail.com>
2025-09-23 16:50:43 +0200

    Revert "Move back to react-router-dom"
    This reverts commit b905f3393c461289434a6fcb7cfce90b367932be.

--- webui/src/components/NavBar.jsx
+++ webui/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { image } from '../actions/utils'
 import {clientPath} from '../constants';
 
